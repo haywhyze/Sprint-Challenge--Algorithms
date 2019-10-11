33db23ad105c5b724b55d71fb3c49b383bf6d26d
Yusuf Abdulkarim <lambda@Yusufs-MacBook-Pro.local>
2019-10-11 11:02:08 +0100

    implement robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,32 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # sorted = False
+        # while not sorted:
+        #     swaps = 0
+        #     for i in range(0, len(arr) - 1):
+        #         if arr[i] > arr[i+1]:
+        #             swaps += 1
+        #             (arr[i+1], arr[i]) = (arr[i], arr[i+1])
+        #     if not swaps:
+        #         sorted = True
+        # return arr
+        while True:
+            self.set_light_off() # start with lights off (no swaps)
+            while self.can_move_left(): # return back to start of loop
+                self.move_left()
+            while self.can_move_right(): # traverse the loop
+                self.swap_item() # pick up item
+                self.move_right(); # move to the right to compare
+                if self.compare_item() == 1: # 
+                    self.set_light_on()
+                    self.swap_item()
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+            if self.light_is_on() == False:
+                break
+        
 
 
 if __name__ == "__main__":
@@ -105,7 +130,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [1,2,7,3,9,6,8,4,5]
     robot = SortingRobot(l)
 
     robot.sort()
